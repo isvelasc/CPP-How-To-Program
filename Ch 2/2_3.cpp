bb648b4bd3b7294fbcfad10d5ed5d876fe567a3f
isvelasc <59635887+isvelasc@users.noreply.github.com>
2021-07-22 19:34:58 -0700

    2.3 - too many endl...

---
+++ Ch 2/2_3.cpp
@@ -0,0 +1,31 @@
+/* =======================================
+*
+*           Exercise 2.3
+*
+*
+* ======================================= */
+
+#include <iostream>
+
+int main() {
+
+   int c, thisIsAVariable, q76345;
+   int age;
+
+   std::cout << "Enter an integer: ";
+   std::cin >> age;
+
+   if (age != 7) {
+      std::cout << "The variable number is not equal to 7." << cout::endl;
+   }
+
+   std::cout << "This is a C++ program" << cout::endl;
+   std::cout << "This is a C++" << cout::endl;
+   std::cout << "program" << cout::endl;
+   std::cout << "This" << cout::endl;
+   std::cout << "is" << cout::endl;
+   std::cout << "a" << cout::endl;
+   std::cout << "C++" << cout::endl;
+   std::cout << "program" << cout::endl;
+   std::cout << "This\tis\ta\tC++\tprogram" << cout::endl;
+}
